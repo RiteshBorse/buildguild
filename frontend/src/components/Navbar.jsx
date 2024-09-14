66199ff8fdd59feb40baa7357a4263905fdd48be
RiteshBorse <riteshborse008@gmail.com>
2024-09-14 16:38:58 +0530

    Navbar Hamburger and Signup Page Modified

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,153 @@
-import React from 'react'
-import { Button } from './ui/button'
-import { Link } from 'react-router-dom';
-const Navbar = ({auth}) => {
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+import { Menu } from "lucide-react";
+import { Input } from "./ui/input";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+const Navbar = ({ auth }) => {
   const user = auth;
-  return (
-    <div className='z-20 font-roboto flex items-center h-fit w-full justify-between px-11 py-4 shadow-lg fixed top-0 left-0 right-0 bg-white'>
-         <Link to="/"><h1 className='text-4xl font-bold'>buildguild</h1></Link>
-        <div className='flex text-xl items-center gap-6'>
-           <Link to="/"> <Button className='text-lg font-light' variant="ghost">Home</Button></Link>
-            <Button className='text-lg font-light' variant="ghost">Features</Button>
-            <Button className='text-lg font-light' variant="ghost">About</Button>
-            <Button className='text-lg font-light' variant="ghost">Contact Us</Button>
-            {
-              !user ? <Button className='text-lg font-light'>Login</Button> : 
-                     <Button className='text-lg font-light' variant='outline'>Logout</Button>
-            }
-           
-        </div>
-    </div>
-  )
-}
+  const [isOpen, setisOpen] = useState(false);
+  const [onClickLogin, setonClickLogin] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setisOpen(!isOpen);
+  };
+
+  const toggleLogin = () => {
+    setonClickLogin(!onClickLogin);
+  };
+  return (
+    <div className="z-20 font-roboto flex items-center h-fit w-full justify-between px-11 py-4 shadow-lg fixed top-0 left-0 right-0 bg-white">
+      <Link to="/">
+        <h1 className="text-4xl font-bold">buildguild</h1>
+      </Link>
+
+      {/* Hamburger Menu */}
+      <Menu
+        className="sm:hidden text-2xl justify-self-end"
+        onClick={toggleMenu}
+      />
+
+      {/* Desktop View */}
+      <div className="hidden md:flex text-xl items-center gap-6">
+        <Link to="/">
+          <Button className="text-lg font-light" variant="ghost">
+            Home
+          </Button>
+        </Link>
+        <Button className="text-lg font-light" variant="ghost">
+          Features
+        </Button>
+        <Button className="text-lg font-light" variant="ghost">
+          About
+        </Button>
+        <Button className="text-lg font-light" variant="ghost">
+          Contact Us
+        </Button>
+        {!user ? (
+          <Link to="/signup">
+            <Button className="text-lg font-light">Login</Button>
+          </Link>
+        ) : (
+          <Button className="text-lg font-light" variant="outline">
+            Logout
+          </Button>
+        )}
+      </div>
+
+      {/* Mobile View */}
+      {isOpen && (
+        <Sheet open={isOpen} onOpenChange={setisOpen}>
+          <SheetContent side="right">
+            <SheetHeader className="flex flex-col mx-auto items-center justify-center my-10  gap-5">
+              <SheetTitle className="text-3xl">
+               buildguild
+              </SheetTitle>
+              <SheetDescription>
+                <div className="flex flex-col text-xl items-center gap-6">
+                  <Link to="/">
+                    <Button
+                      onClick={toggleMenu}
+                      className="text-lg font-light"
+                      variant="ghost"
+                    >
+                      Home
+                    </Button>
+                  </Link>
+                  <Button
+                    onClick={toggleMenu}
+                    className="text-lg font-light"
+                    variant="ghost"
+                  >
+                    Features
+                  </Button>
+                  <Button
+                    onClick={toggleMenu}
+                    className="text-lg font-light"
+                    variant="ghost"
+                  >
+                    About
+                  </Button>
+                  <Button
+                    onClick={toggleMenu}
+                    className="text-lg font-light"
+                    variant="ghost"
+                  >
+                    Contact Us
+                  </Button>
+                  {!user ? (
+                    <Button
+                      onClick={()=>{
+                        toggleMenu(); 
+                        toggleLogin();
+                      }
+                      }
+                      className="text-lg font-light"
+                    >
+                      Login
+                    </Button>
+                  ) : (
+                    <Button
+                      onClick={() => {
+                        toggleMenu, onClickLogin;
+                      }}
+                      className="text-lg font-light"
+                      variant="outline"
+                    >
+                      Logout
+                    </Button>
+                  )}
+                </div>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      )}
+
+      {onClickLogin && (
+        <Sheet open={onClickLogin} onOpenChange={setonClickLogin}>
+          <SheetContent side="bottom">
+            <SheetHeader className="flex flex-col mx-auto items-center justify-center w-[80%] sm:w-1/3 my-10  gap-5">
+              <SheetTitle className="text-3xl">
+              Login
+              </SheetTitle>
+              <SheetDescription className="flex flex-col w-full gap-6">
+                <Input placeholder="Username" />
+                <Input placeholder="Password" />
+                <Button>Login</Button>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,47 +1,48 @@
 import React from "react";
 import { Input } from "@/components/ui/input";
-import { CiSearch } from "react-icons/ci";
 import { Button } from "@/components/ui/button";
 import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 import { toast } from "sonner";
-
 const Signup = () => {
   return (
     <>
-      <div className="h-fit px-16 py-10 w-full pt-[100px] bg-[#151515] text-white flex flex-col gap-10">
+      <div className="h-fit px-16 py-10 w-full pt-[100px] min-h-screen bg-[#151515] text-white flex flex-col gap-10">
         <h1 className="text-4xl font-bold ">Sign Up</h1>
         <div className="flex justify-around self-start gap-8 w-2/3 mt-2 flex-col">
           <div className="flex flex-col sm:flex-row gap-6">
-            <Input placeholder="Name" />
-            <Input placeholder="Middle Name" />
-            <Input placeholder="Last Name" />
+            <Input style="custom" placeholder="Name" />
+            <Input style="custom" placeholder="Middle Name" />
+            <Input style="custom" placeholder="Last Name" />
           </div>
           <div className="flex flex-col sm:flex-row  gap-6">
-            <Input type="email" placeholder="Email" />
-            <div className="flex items-center relative">
-              <Input placeholder="Username"/>
-              <CiSearch className="text-white h-full rounded-md w-9 p-2 absolute self-center right-0 hover:bg-[#2c2c2c]" />
-            </div>
-            <Input placeholder="OTP" />
+            <Input style="custom" type="email" placeholder="Email" />
+            <Input style="custom" type="text" placeholder="Username" />
+            <Input style="custom" placeholder="OTP" />
           </div>
           <div className="flex flex-col sm:flex-row  gap-6">
-            <Input placeholder="City" />
-            <Input placeholder="State" />
-            <Input placeholder="Country" />
+            <Input style="custom" placeholder="City" />
+            <Input style="custom" placeholder="State" />
+            <Input style="custom" placeholder="Country" />
           </div>
           <div className="flex flex-col sm:flex-row  gap-6 self-start">
-            <Input type="password" placeholder="Set Password"></Input>
-            <Input type="password" placeholder="Confirm Password"></Input>
+            <Input
+              style="custom"
+              type="password"
+              placeholder="Set Password"
+            ></Input>
+            <Input
+              style="custom"
+              type="password"
+              placeholder="Confirm Password"
+            ></Input>
           </div>
         </div>
         <div className="flex flex-col self-center">
@@ -51,29 +52,28 @@ const Signup = () => {
           >
             SignUp
           </Button>
-          <p className="text-lg">
-            Already have an account ?
-            <Drawer>
-              <DrawerTrigger>
-                <Button className="dark text-lg" variant="link">
+          <Sheet>
+            <p className="text-lg">
+              Already have an account ?
+              <SheetTrigger>
+                <Button className="text-white font-bold text-lg" variant="link">
                   Login
                 </Button>
-              </DrawerTrigger>
-              <DrawerContent className='flex items-center'>
-                
-                  <h1 className="text-3xl font-bold">Login</h1>
-                  <input className="outline-none border-[1px] border-black px-2 h-10 w-[70%] sm:w-[25%] m-4 rounded-md " type='text' placeholder="Enter Username"></input>
-                  <input className=" outline-none border-[1px] border-black px-2 h-10 w-[70%] sm:w-[25%] m-4 rounded-md" type='password' placeholder="Enter Password"></input>
-             
-                <DrawerFooter className="flex flex-row sm:flex-col py-5">
+              </SheetTrigger>
+            </p>
+            <SheetContent side="bottom">
+              <SheetHeader className="flex flex-col mx-auto items-center justify-center w-[80%] sm:w-1/3 my-10  gap-5">
+                <SheetTitle>
+                  <p className="text-3xl">Login</p>
+                </SheetTitle>
+                <SheetDescription className="flex flex-col w-full gap-6">
+                  <Input placeholder="Username" />
+                  <Input placeholder="Password" />
                   <Button>Login</Button>
-                  <DrawerClose>
-                    <Button variant="outline">Cancel</Button>
-                  </DrawerClose>
-                </DrawerFooter>
-              </DrawerContent>
-            </Drawer>
-          </p>
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </>
