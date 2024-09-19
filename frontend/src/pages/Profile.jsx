58ec4d90b5342426163591d43412c6bd46a99cc1
RiteshBorse <riteshborse008@gmail.com>
2024-09-19 18:44:31 +0530

    Profile Section updated

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,36 +1,53 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { signUp } from '@/schema/signUpSchema'
+import { apiVerify } from '@/schema/apiSchema'
+import axios from 'axios'
 import React , {useState} from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 
 const Profile = () => {
-    const [data, setdata] = useState({})
-    const [errors, seterrors] = useState({})
     const { register , handleSubmit  } = useForm();
-    const onSubmit = (data) => {
-        const result = signUp.safeParse(data);
-        if (!result.success) {
-          const fieldError = result.error.formErrors.fieldErrors;
-          seterrors(fieldError);
+    const onSubmit = async (data) => {
+      try {
+        const res = await axios.patch(
+          `${import.meta.env.VITE_API_URL}/api/user/profile`,
+          data
+        );
+        if (!apiVerify(res)) {
+          toast.warning("Api Error , Please contact admin");
           return;
         }
-        setdata(data);
-        seterrors({});
+        toast.success(res.data.message);
+      } catch (error) {
+        console.log(error)
+        const { response } = error;
+        if (!response) {
+          toast.error("Database connection error");
+          return;
+        }
+        if (!apiVerify(response)) {
+          toast.warning("Api Error , Please contact admin");
+          return;
+        }
+        toast.error(response.data.message);
+      }
     }
   return (
     <div className='w-full h-screen pt-[100px] px-16 py-10 flex flex-col'>
-      <form action="" className='flex flex-col gap-4 w-2/3 self-center'>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4 w-2/3 self-center'>
         <h1 className='text-4xl '>Profile</h1>
-        <Input placeholder="Enter First Name"/>
-        <Input placeholder="Enter Middle Name"/>
-        <Input placeholder="Enter Last Name"/>
-        <Input placeholder="Enter Email"/>
-        <Input placeholder="Enter Username"/>
-        <Input placeholder="Enter City"/>
-        <Input placeholder="Enter State"/>
-        <Input placeholder="Enter Country"/>
-        <Button className="mt-4 w-1/3 self-end">Update Profile</Button>
+        <Input placeholder="Enter First Name" {...register("firstName")}/>
+        <Input placeholder="Enter Middle Name"{...register("middleName")} />
+        <Input placeholder="Enter Last Name" {...register("lastName")}/>
+        <Input placeholder="Enter Username" {...register("username")}/>
+        <Input placeholder="Enter Email" {...register("email")}/>
+        <Input placeholder="Enter City" {...register("city")}/>
+        <Input placeholder="Enter State" {...register("state")}/>
+        <Input placeholder="Enter Country" {...register("country")}/>
+        <Input placeholder="Enter Password" {...register("password")}/>
+        
+        <Button type="submit" className="mt-4 w-1/3 self-end">Update Profile</Button>
       </form>
     </div>
   )
