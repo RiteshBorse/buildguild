27f25d1774a47731afe4fc4aca0d99df9296eaeb
Ritesh Borse <121285250+RiteshBorse@users.noreply.github.com>
2025-01-04 22:14:27 +0530

    Only for testing purpose

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -12,7 +12,7 @@ import { uploadFile } from "../middleware/cloudinary.middleware.js";
 
 const router = Router();
 router.route("/myProjects").get(authenticate, getMyProjects);
-router.route("/createProject").post(authenticate ,upload , uploadFile, createProject);
+router.route("/createProject").post(upload , uploadFile, createProject);
 router.route("/deleteProject/:projectId").delete(authenticate, deleteProject);
 router.route("/publish/:id").get(authenticate , publishProject);
 router.route("/getProject/:id").get(authenticate , getProject)
